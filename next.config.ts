9f5be0e73b46951ab61d093ad581d9a3336174d7
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-11-18 18:35:43 +0000

    mantra

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/adnixmedia' : ''
+  basePath: process.env.NODE_ENV === 'production' ? '/adnixmedia' : '',
   output: "export",
   reactStrictMode: true,
   images: {
