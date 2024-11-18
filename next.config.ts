d0cfe94960afc1cebc64e1804083e6d61d859494
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-11-18 13:24:39 +0000

    ertgyhuj

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/adnixmedia/' : '',
-  basePath: isProd ? '/adnixmedia' : '',
+  basePath: isProd ? '/adnixmedia',
   output: 'export'
 };
 
